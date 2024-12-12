2a461b0c0134af2fcdef6f44ac7994ac71a64602
Salmin Lev <prostoweb123@gmail.com>
2024-12-13 02:01:43 +0600

    Create store gift method

---
+++ www/app/models/GiftModel.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\models;
+
+use core\Model;
+
+class GiftModel extends Model
+{
+    public string $tableName = 'estatepool_gifts';
+}
--- www/public/index.php
+++ www/public/index.php
@@ -2,6 +2,7 @@
 // Autoload core files using namespaces and PSR-4 autoloading
 use app\controllers\HomeController;
 use app\controllers\PoolController;
+use app\controllers\GiftController;
 use core\Router;
 
 spl_autoload_register(function($class) {
@@ -17,6 +18,7 @@ $router = new Router();
 $router->add('/', [new HomeController(), 'index']);
 $router->add('/api/v1/pool', [new PoolController(), 'index']);
 $router->add('/api/v1/pool/create', [new PoolController(), 'store']);
+$router->add('/api/v1/gift/create', [new GiftController(), 'store']);
 
 
 // Get the requested URL
